989881659d586ad991235f83e34a12312c27b26e
pewresouce <154728100+pewresouce@users.noreply.github.com>
2025-07-19 12:18:48 +0100

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Install dependencies required for Puppeteer
+apt-get update && apt-get install -y \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libgdk-pixbuf2.0-0 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils \
+  libgbm1 \
+  libpango-1.0-0 \
+  libpangocairo-1.0-0 \
+  libxss1 \
+  libxtst6
+# Install Node.js dependencies
+npm install
